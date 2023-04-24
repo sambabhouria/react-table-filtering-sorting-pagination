be35d4e56c2f506b979437cfcc29f9d3aa06501d
Mamadou Samba DIALLO <sdiallo@MacBook-Pro-de-ENEANCE-2.local>
2023-04-24 11:52:58 +0200

    react table zero lib

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+import { Table } from "./Table";
+
+export default function App() {
+  const columns = [
+    { accessor: "name", label: "Name" },
+    { accessor: "age", label: "Age" },
+    {
+      accessor: "is_manager",
+      label: "Manager",
+      format: (value) => (value ? "✔️" : "✖️"),
+    },
+    { accessor: "start_date", label: "Start Date" },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      name: "Liz Lemon",
+      age: 36,
+      is_manager: true,
+      start_date: "02-28-1999",
+    },
+    {
+      id: 2,
+      name: "Jack Donaghy",
+      age: 40,
+      is_manager: true,
+      start_date: "03-05-1997",
+    },
+    {
+      id: 3,
+      name: "Tracy Morgan",
+      age: 39,
+      is_manager: false,
+      start_date: "07-12-2002",
+    },
+    {
+      id: 4,
+      name: "Jenna Maroney",
+      age: 40,
+      is_manager: false,
+      start_date: "02-28-1999",
+    },
+    {
+      id: 5,
+      name: "Kenneth Parcell",
+      age: Infinity,
+      is_manager: false,
+      start_date: "01-01-1970",
+    },
+    {
+      id: 6,
+      name: "Pete Hornberger",
+      age: 42,
+      is_manager: true,
+      start_date: "04-01-2000",
+    },
+    {
+      id: 7,
+      name: "Frank Rossitano",
+      age: 36,
+      is_manager: false,
+      start_date: "06-09-2004",
+    },
+    { id: 8, name: null, age: null, is_manager: null, start_date: null },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Table</h1>
+      <h2>Sorting, Filtering, Pagination</h2>
+
+      <Table rows={rows} columns={columns} />
     </div>
   );
 }
-
-export default App;
